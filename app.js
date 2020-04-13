7448d60afe85521ae8ef80102e45dc752c4182ca
devatuteja <58606366+devatuteja@users.noreply.github.com>
2020-04-13 21:38:44 +0530

    first code

---
+++ app.js
@@ -0,0 +1,8 @@
+var myview = new Vue({
+    el: "#our-app",
+    data: {
+        message: "this is our app popoye the sailor man",
+        fnmae: "dev",
+        channel: "online web tutors"
+    }
+});

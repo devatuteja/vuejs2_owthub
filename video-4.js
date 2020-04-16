e680b4bd1ff53e0b68231826cf52d15961bfbaa3
devatuteja <58606366+devatuteja@users.noreply.github.com>
2020-04-16 16:19:13 +0530

    Add files via upload

---
+++ video-4.js
@@ -0,0 +1,16 @@
+var myapp = new Vue({
+    el: "#my-app",
+    data: {
+        name: "Web tutorials",
+        ulink: "https://www.youtube.com/watch?v=BqCaoGuRDC4&list=PLT9miexWCpPUZ-xD9s70PhhP37blPSuax&index=4",
+        title: "This is Channel Link",
+        ImageLink: "gallery1.jpg",
+        isActive: true,
+        txtName : "Sanjay Kumar",
+        txtEmail:"sanjay@gmail.com",
+        htmlContent: "<p>This is my paragraph set from js file</p> "
+
+
+
+    }
+});
